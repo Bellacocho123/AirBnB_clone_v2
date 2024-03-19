ae4c88d6dfbea13daa81575a938f84b70fa07967
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-19 21:25:37 +0100

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy import Column, String
-from os import getenv
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel):
+    name = ""
--- models/review.py
+++ models/review.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
-from os import getenv
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'reviews'
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    else:
-        text = ""
-        place_id = ""
-        user_id = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes review"""
-        super().__init__(*args, **kwargs)
+    place_id = ""
+    user_id = ""
+    text = ""
