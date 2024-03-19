7319f7c908b54990610540904e5240e647d7cfe2
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-19 21:52:34 +0100

    update

--- models/place.py
+++ models/place.py
@@ -6,6 +6,18 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
+if getenv("HBNB_TYPE_STORAGE") == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id', onupdate='CASCADE',
+                                            ondelete='CASCADE'),
+                                 primary_key=True))
+
+
 class Place(BaseModel, Base):
     """ A place to stay """
     if getenv("HBNB_TYPE_STORAGE") == "db":
@@ -20,6 +32,10 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place")
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 backref="place_amenities",
+                                 viewonly=False)
     else:
         city_id = ""
         user_id = ""
