c5defdb4fa79baa9966abc6d2ac2fa5e9db4f9f1
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-19 21:46:26 +0100

    update

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
@@ -15,11 +16,24 @@ class User(BaseModel, Base):
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def reviews(self):
+            """getter attribute returns the list of Review instances"""
+            from models.review import Review
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
