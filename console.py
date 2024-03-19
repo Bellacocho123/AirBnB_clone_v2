29fe73648cf9376535683a3d9ff5f8cd82151fb9
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-19 21:06:18 +0100

    test

--- console.py
+++ console.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Console Module  """
 import cmd
+import shlex
 import sys
 import uuid
+import models
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -12,6 +14,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 from datetime import datetime
+from os import getenv
 
 
 def parse_args(args):
@@ -166,7 +169,7 @@ class HBNBCommand(cmd.Cmd):
             kwargs['__class__'] = params[0]
             kwargs['id'] = str(uuid.uuid4())
         new_instance = HBNBCommand.classes[params[0]](**kwargs)
-        storage.save()
+        new_instance.save()
         print(new_instance.id)
 
     def help_create(self):
@@ -242,21 +245,20 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
-        print_list = []
-
-        if args:
-            args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+        args = shlex.split(args)
+        obj_list = []
+        if len(args) == 0:
+            obj_dict = models.storage.all()
+        elif args[0] in HBNBCommand.classes:
+            obj_dict = models.storage.all(HBNBCommand.classes[args[0]])
         else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
-
-        print(print_list)
+            print("** class doesn't exist **")
+            return False
+        for key in obj_dict:
+            obj_list.append(str(obj_dict[key]))
+        print("[", end="")
+        print(", ".join(obj_list), end="")
+        print("]")
 
     def help_all(self):
         """ Help information for the all command """
--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City"""
+            from models import storage
+            from models.city import City
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
