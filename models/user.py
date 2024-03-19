652bdc3200e79616f9977dea0fd51b48533cba8d
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-19 21:32:12 +0100

    update

--- models/user.py
+++ models/user.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -12,6 +14,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
     else:
         email = ""
         password = ""
