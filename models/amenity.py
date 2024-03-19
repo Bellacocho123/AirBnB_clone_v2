50fc97897ac99b8abdca4c60c4841b869ac58130
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-19 21:21:36 +0100

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity """
-    if models.storage_t == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
     else:
