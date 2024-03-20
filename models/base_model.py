9d5d46a3b7f9f9bddc80b4dc1567e61b5a6030f6
Adewale Kujore <adewalekujore6@gmail.com>
2024-03-20 09:52:53 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ if getenv("HBNB_TYPE_STORAGE") == "db":
     Base = declarative_base()
 else:
     Base = object
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
@@ -19,30 +20,28 @@ class BaseModel:
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kw):
         """Instatntiates a new model"""
-        if not kwargs:
+        if not kw:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            if 'id' not in kwargs:
+            if 'updated_at' in kw:
+                kw['updated_at'] = datetime.strptime(kw['updated_at'], time)
+            if 'created_at' in kw:
+                kw['created_at'] = datetime.strptime(kw['created_at'], time)
+            if 'id' not in kw:
                 self.id = str(uuid.uuid4())
-            if '__class__' in kwargs:
-                del kwargs['__class__']
-            if 'updated_at' not in kwargs:
+            if '__class__' in kw:
+                del kw['__class__']
+            if 'updated_at' not in kw:
                 self.updated_at = datetime.now()
-            if 'created_at' not in kwargs:
+            if 'created_at' not in kw:
                 self.created_at = datetime.now()
-            for key, value in kwargs.items():
+            for key, value in kw.items():
                 setattr(self, key, value)
-            self.__dict__.update(kwargs)
+            self.__dict__.update(kw)
 
     def __str__(self):
         """Returns a string representation of the instance"""
