854410710a874b3838c6a9d2dacde4a5289db252
bellacocho123 <isabelsky59@gmail.com>
2024-04-08 17:34:23 -0700

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,22 +26,3 @@ sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
-
-# Sets up web server for deployment of web_static
-
-apt update
-apt install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-
-echo "Holberton School" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu:ubuntu /data/
-
-sed -i "38i \ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-
-service nginx restart
-
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,34 +28,4 @@ def do_deploy(archive_path):
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
-
-# Distributes an archive to your web servers, using the function do_deploy
-
-
-def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    from fabric.api import env, put, run
-    from os.path import exists
-    env.hosts = ["100.25.118.136", "54.160.105.221"]
-
-    if exists(archive_path) is False:
-        return False
-    try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/" +
-            archive_path[9:-4] + "/")
-        run("tar -xzf /tmp/" + archive_path[9:] +
-            " -C /data/web_static/releases/" +
-            archive_path[9:-4] + "/")
-        run("rm /tmp/" + archive_path[9:])
-        run("mv /data/web_static/releases/" +
-            archive_path[9:-4] + "/web_static/* /data/web_static/releases/" +
-            archive_path[9:-4] + "/")
-        run("rm -rf /data/web_static/releases/" +
-            archive_path[9:-4] + "/web_static")
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/" +
-            archive_path[9:-4] + "/ /data/web_static/current")
-        return True
-    except Exception:
         return False
